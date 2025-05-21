038cba0118dc2d94478848b411b719e57fc43ec0
Alp Tahta <alp.tht48@gmail.com>
2025-05-21 23:49:41 +0300

    init game with ebiten

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"github.com/alp-tahta/tiablo.git/internal/logger"
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
+}
 
 func main() {
-	fmt.Println("test")
+	l := logger.Init()
+	g := &Game{}
+
+	err := ebiten.RunGame(g)
+	if err != nil {
+		l.Error(err.Error())
+	}
 }
